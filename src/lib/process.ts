4b56e85ab545e25d2ccf6b81062ed6d41c9b7c69
Sercrac07 <sercassan@gmail.com>
2024-08-22 15:18:47 +0200

    style: format code

--- src/lib/process.ts
+++ src/lib/process.ts
@@ -2,10 +2,10 @@ export function detectPackageManager() {
   const userAgent = process.env.npm_config_user_agent
   if (userAgent) {
     // if (userAgent.includes('yarn')) return 'yarn'
-    if (userAgent.includes('pnpm')) return 'pnpm'
+    if (userAgent.includes("pnpm")) return "pnpm"
     // if (userAgent.includes('bun')) return 'bun'
-    if (userAgent.includes('npm')) return 'npm'
+    if (userAgent.includes("npm")) return "npm"
   }
-  if (process.argv[0].includes('npx') || process.argv.some(arg => arg.includes('npx'))) return 'npm'
-  return 'npm'
+  if (process.argv[0].includes("npx") || process.argv.some(arg => arg.includes("npx"))) return "npm"
+  return "npm"
 }
